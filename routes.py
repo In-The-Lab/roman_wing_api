b05fb3a02f405e29472fb0b0a0c979ef427cb449
Vedant Pathak <vedant_pathak@Vedants-MacBook-Pro.local>
2018-07-25 19:12:28 -0500

    added a commet below to help out

--- routes.py
+++ routes.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+'''
+	This here is how you create a route.
+	This specific route is "http://localhost:5000/".
+	You can see that this route only accepts GET requests.
+	In order to accept POST requests, you would add "POST"
+	to the list specified below.
+'''
 @app.route("/", methods=["GET"])
 def do_nothing():
 	return "<h1>Hello, world!</h1>"
